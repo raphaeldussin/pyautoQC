23273a25e4d3eb5157f827aae1e9086ae66132b4
Raphael Dussin <raphael.dussin@gmail.com>
2019-07-15 13:21:57 -0400

    wrapper for data checks

--- pyautoQC/dataQC.py
+++ pyautoQC/dataQC.py
@@ -11,6 +11,14 @@ def check_masksize(da, spval=1e+15, x='lon', y='lat', z='lev', time='time'):
     masked = da.fillna(spval)
     # compute the size of the mask in function of depth and time
     masksize = masked.where(masked == spval).count(dim=[x, y])
+    # Check that land sea mask size has reasonable value
+    masksize_surf = masksize.isel({z: 0, time: 0})
+    # land covers 29% of earth surface, we allow 10% error
+    expected = 0.29 * da[x].size * da[y].size
+    if not (0.9 * expected) < masksize_surf < (1.1 * expected):
+        check = False
+        message = 'PROBLEM: mask size is not realistic'
+        return check, message
     # Check that the size does not change over time
     masksize_3d = masksize.sum(dim=z)
     tendency = masksize_3d.diff(dim=time)
@@ -24,6 +32,8 @@ def check_masksize(da, spval=1e+15, x='lon', y='lat', z='lev', time='time'):
         check = False
         message = 'PROBLEM: mask size is decreasing with depth'
         return check, message
+    # normal case
+    return check, message
 
 
 def check_timeaxis(ds, time='time'):
@@ -35,4 +45,32 @@ def check_timeaxis(ds, time='time'):
     if not np.timedelta64(28, 'D') <= tendency <= np.timedelta64(31, 'D'):
         check = False
         message = 'PROBLEM: records are not correctly spaced'
+    return check, message
+
+
+def check_second_derivative(da, x='lon', y='lat', z='lev', time='time'):
+    """ check for zero second derivative """
+
+    check = True
+    message = ''
+
+    curv_x = da.diff(dim=x, n=2)
+    curv_y = da.diff(dim=y, n=2)
+    curv_z = da.diff(dim=z, n=2)
+
+    if curv_x.values.any() == 0:
+        check = False
+        message = 'PROBLEM: contiguous values along x axis'
         return check, message
+
+    if curv_y.values.any() == 0:
+        check = False
+        message = 'PROBLEM: contiguous values along y axis'
+        return check, message
+
+    if curv_z.values.any() == 0:
+        check = False
+        message = 'PROBLEM: contiguous values along z axis'
+        return check, message
+    # normal case
+    return check, message
