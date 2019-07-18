2fa1f583e3db12e0db5880c133c457c1960b27c1
Raphael Dussin <raphael.dussin@gmail.com>
2019-07-18 13:29:01 -0400

    add tests

---
+++ pyautoQC/test/test_dataQC.py
@@ -0,0 +1,72 @@
+import xarray as xr
+import pandas as pd
+import numpy as np
+
+# TEST DATASETS
+lon1 = np.arange(0, 360)
+lat1 = np.arange(-90, 90)
+z = np.arange(15)
+lon, lat = np.meshgrid(lon1, lat1)
+temp = 20 * np.cos(np.pi * lat / 180) * np.ones((24, 15,) + lon.shape)
+for k in np.arange(15):
+    temp[:, k, :, :] = temp[:, k, :, :] * np.exp(-k/15)
+temp_m = np.ma.masked_less(temp, 8)
+temp_m_rand = temp_m + np.random.rand(24, 15, 180, 360)
+
+
+# REFERENCE
+ds_ref = xr.Dataset({'temperature': (['time', 'z', 'y', 'x'], temp_m_rand)},
+                    coords={'lon': (['y', 'x'], lon),
+                            'lat': (['y', 'x'], lat),
+                            'z': (['z'], z),
+                            'time': pd.date_range(start='1900-1-1',
+                                                  periods=24, freq='1M')})
+
+# BAD TIME AXIS
+ds_test01 = ds_ref.copy(deep=True)
+ds_test01['time'].values[12:] = 0.
+
+ds_test02 = ds_ref.copy(deep=True)
+ds_test02['time'] = pd.date_range(start='1900-1-1', periods=24, freq='2M')
+
+# REPEATED VALUES ON X AXIS
+ds_test03 = ds_ref.copy(deep=True)
+ds_test03['temperature'].values[:] = temp_m
+
+
+def test_check_timeaxis():
+    from pyautoQC.dataQC import check_timeaxis
+
+    check, message = check_timeaxis(ds_ref)
+    assert check
+    assert message == ''
+
+    check, message = check_timeaxis(ds_test01)
+    assert not check
+    assert message != ''
+
+    check, message = check_timeaxis(ds_test02)
+    assert not check
+    assert message != ''
+
+
+def test_check_masksize():
+    from pyautoQC.dataQC import check_masksize
+
+    check, message = check_masksize(ds_ref['temperature'], x='x', y='y', z='z')
+    assert check
+    assert message == ''
+
+
+def test_check_second_derivative():
+    from pyautoQC.dataQC import check_second_derivative
+
+    check, message = check_second_derivative(ds_ref['temperature'],
+                                             x='x', y='y', z='z')
+    assert check
+    assert message == ''
+
+    check, message = check_second_derivative(ds_test03['temperature'],
+                                             x='x', y='y', z='z')
+    assert not check
+    assert message != ''
