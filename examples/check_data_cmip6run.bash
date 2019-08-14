ba45ef7b04ef025913fda25600d0a682b7c51973
Raphael Dussin <Raphael.Dussin@ldt-1467436.gfdl.noaa.gov>
2019-08-14 13:59:55 -0400

    update script

--- examples/check_data_cmip6run.bash
+++ examples/check_data_cmip6run.bash
@@ -4,10 +4,14 @@
 conda activate devQC
 
 source_id=GFDL-ESM4
-exp_id=piControl
+#exp_id=piControl
+#exp_id=abrupt-4xCO2
+exp_id=1pctCO2
+onlypostcheck=False
 dirrun=/data_cmip6/CMIP6/CMIP/NOAA-GFDL/${source_id}/${exp_id}/r1i1p1f1/Omon
 dirout=/work/Raphael.Dussin/QC_results/${source_id}-${exp_id}_Omon
 dirpng=/work/Raphael.Dussin/QC_results/${source_id}-${exp_id}_Omon/pngs
+csvout=./csv/${source_id}-${exp_id}_Omon.csv
 
 mkdir -p $dirout
 mkdir -p $dirpng
@@ -30,22 +34,24 @@ for var in $listvar ; do
 
       echo working on var $var on grid $grid in tag $tag
 
-      # option 1: one test per file
-      for file in $files ; do
-        echo working on file $file 
-        if [ $grid == gn ] ; then
-           time qc_check_data -l $file -v $var -t 1 -x x -y y -z lev -w 6 -o $dirout
-           rm -f slurm*out
-        elif [ $grid == gr ] ; then
-           time qc_check_data -l $file -v $var -t 1 -x lon -y lat -z lev -w 4 -o $dirout
-           rm -f slurm*out
-        elif [ $grid == gr1 ] ; then
-           time qc_check_data -l $file -v $var -t 1 -x lon -y lat -z lev -w 2 -o $dirout
-           rm -f slurm*out
-        else
-           echo PROBLEM: not sure how to treat grid $grid
-        fi
-      done
+      if [ $onlypostcheck != 'True' ] ; then
+        # option 1: one test per file
+        for file in $files ; do
+          echo working on file $file 
+          if [ $grid == gn ] ; then
+             time qc_check_data -l $file -v $var -t 1 -x x -y y -z lev -w 2 -o $dirout -c $csvout
+             rm -f slurm*out
+          elif [ $grid == gr ] ; then
+             time qc_check_data -l $file -v $var -t 1 -x lon -y lat -z lev -w 2 -o $dirout -c $csvout
+             rm -f slurm*out
+          elif [ $grid == gr1 ] ; then
+             time qc_check_data -l $file -v $var -t 1 -x lon -y lat -z lev -w 2 -o $dirout -c $csvout
+             rm -f slurm*out
+          else
+             echo PROBLEM: not sure how to treat grid $grid
+          fi
+        done
+      fi
 
       tagout=${source_id}-${exp_id}_${grid} 
       echo checking for outliers in mean timeserie
